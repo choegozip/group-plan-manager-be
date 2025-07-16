a7c0541c70dc2d34dc8e0cc85190a8e1f1ea747b
goo3oo <goo3oo0918@gmail.com>
2025-07-16 21:33:46 +0900

    feat: 로케일 응답 구현, 성공/에러 메시지 추가 및 통합

--- src/main/java/com/groupplanmanagerbe/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/groupplanmanagerbe/global/exception/GlobalExceptionHandler.java
@@ -3,7 +3,10 @@ package com.groupplanmanagerbe.global.exception;
 import com.groupplanmanagerbe.global.common.enums.ApiErrorCode;
 import com.groupplanmanagerbe.global.common.response.ApiErrorRes;
 import com.groupplanmanagerbe.global.exception.custom.CustomRuntimeException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -17,56 +20,66 @@ import java.nio.file.AccessDeniedException;
 
 @Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+    private final MessageSource messageSource;
 
     @ExceptionHandler(CustomRuntimeException.class)
     protected ResponseEntity<ApiErrorRes> handleCustomException(
             final CustomRuntimeException e) {
-        log.warn("Custom Exception: {}", e.getMessage());
-        return createResponseEntity(ApiErrorRes.of(e.getErrorCode()));
+        log.warn("예외 발생: {}", e.getMessage());
+        String localizedMessage = messageSource.getMessage(
+                e.getErrorCode().getMessageKey(), null, LocaleContextHolder.getLocale());
+        return createResponseEntity(ApiErrorRes.of(e.getErrorCode().getHttpStatus(),localizedMessage));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ApiErrorRes> handleMethodArgumentNotValidException(
             final MethodArgumentNotValidException e) {
         log.warn("벨리데이션 오류: {}", e.getMessage());
-        ApiErrorRes response = new ApiErrorRes(HttpStatus.BAD_REQUEST,
-                e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
-        return createResponseEntity(response);
+        String defaultMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        String localizedMessage =
+                messageSource.getMessage(defaultMessage, null, defaultMessage, LocaleContextHolder.getLocale());
+        return createResponseEntity(ApiErrorRes.of(HttpStatus.BAD_REQUEST, localizedMessage));
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ApiErrorRes> handleAccessDeniedException(
             AccessDeniedException e) {
         log.warn("접근 권한 없음: {}", e.getMessage());
-        return createResponseEntity(ApiErrorRes.of(ApiErrorCode.FORBIDDEN_ACCESS));
+        String localizedMessage = messageSource.getMessage(
+                ApiErrorCode.FORBIDDEN_ACCESS.getMessageKey(), null, LocaleContextHolder.getLocale());
+        return createResponseEntity(ApiErrorRes.of(HttpStatus.FORBIDDEN, localizedMessage));
     }
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<ApiErrorRes> handleAuthenticationException(
             AuthenticationException e) {
         log.warn("인증 실패: {}", e.getMessage());
-
         ApiErrorCode errorCode = ApiErrorCode.UNAUTHORIZED_ACCESS;
         if (e instanceof BadCredentialsException) {
             errorCode = ApiErrorCode.LOGIN_FAILED;
         }
-
-        return createResponseEntity(ApiErrorRes.of(errorCode));
+        String localizedMessage = messageSource.getMessage(
+                errorCode.getMessageKey(), null, LocaleContextHolder.getLocale());
+        return createResponseEntity(ApiErrorRes.of(errorCode.getHttpStatus() ,localizedMessage));
     }
 
     @ExceptionHandler(IOException.class)
     public ResponseEntity<ApiErrorRes> handleIOException(IOException e){
         log.warn("IO Exception: {}", e.getMessage());
-        ApiErrorRes errorResponse = new ApiErrorRes(HttpStatus.BAD_REQUEST, e.getMessage());
-        return createResponseEntity(errorResponse);
+        String localizedMessage = messageSource.getMessage(
+                ApiErrorCode.INTERNAL_SERVER_ERROR.getMessageKey(), null, LocaleContextHolder.getLocale());
+        return createResponseEntity(ApiErrorRes.of(HttpStatus.INTERNAL_SERVER_ERROR, localizedMessage));
     }
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ApiErrorRes> handleException(final Exception e) {
         log.error("Unexpected error occurred", e);  // 예상치 못한 에러는 ERROR 레벨로
-        ApiErrorRes response = new ApiErrorRes(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
-        return createResponseEntity(response);
+        String localizedMessage = messageSource.getMessage(
+                ApiErrorCode.INTERNAL_SERVER_ERROR.getMessageKey(), null, LocaleContextHolder.getLocale());
+        return createResponseEntity(ApiErrorRes.of(HttpStatus.INTERNAL_SERVER_ERROR, localizedMessage));
     }
 
     private ResponseEntity<ApiErrorRes> createResponseEntity(ApiErrorRes response) {
