0874643846400ecf4105e48f78dd70f50cd5e97b
goo3oo <goo3oo0918@gmail.com>
2025-07-16 21:28:22 +0900

    feat: 로케일 응답 구현, 성공/에러 메시지 추가 및 통합

--- src/main/java/com/groupplanmanagerbe/global/common/response/ApiErrorRes.java
+++ src/main/java/com/groupplanmanagerbe/global/common/response/ApiErrorRes.java
@@ -1,11 +1,11 @@
 package com.groupplanmanagerbe.global.common.response;
 
-import com.groupplanmanagerbe.global.common.enums.ApiErrorCode;
 import org.springframework.http.HttpStatus;
 
-public record ApiErrorRes(HttpStatus httpStatus, String message) {
-    public static ApiErrorRes of(ApiErrorCode errorCode) {
-        return new ApiErrorRes(errorCode.getHttpStatus(), errorCode.getMessage());
+public record ApiErrorRes(HttpStatus httpStatus, String messageKey) {
+
+    public static ApiErrorRes of(HttpStatus httpStatus, String message) {
+        return new ApiErrorRes(httpStatus, message);
     }
 }
 
