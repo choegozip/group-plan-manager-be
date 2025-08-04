6a07df64def44d59e2564f38db7708c13211663c
goo3oo <goo3oo0918@gmail.com>
2025-08-04 17:43:32 +0900

    fix: 살 것 수정시 title의 NotBlank 제거

--- src/main/java/com/groupplanmanagerbe/presentation/tobuyitem/dto/request/UpdateToBuyReq.java
+++ src/main/java/com/groupplanmanagerbe/presentation/tobuyitem/dto/request/UpdateToBuyReq.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.*;
 import java.util.List;
 
 public record UpdateToBuyReq(
-        @NotBlank(message = ItemValidationConst.TITLE_BLANK_MESSAGE)
         String title,
 
         @Min(value = ItemValidationConst.QUANTITY_MIN, message = ItemValidationConst.QUANTITY_RANGE_MESSAGE)
