4e2031452ac388781427b352bed7ee3252d2ca4d
goo3oo <goo3oo0918@gmail.com>
2025-09-06 11:01:55 +0900

    refactor: fmc 타이틀, 바디 생성을 위한 이넘 변경

--- src/main/java/com/groupplanmanagerbe/global/common/enums/ManagerStatus.java
+++ src/main/java/com/groupplanmanagerbe/global/common/enums/ManagerStatus.java
@@ -6,7 +6,16 @@ import lombok.Getter;
 @Getter
 public enum ManagerStatus {
 
-    PENDING, ACCEPT, DENY, DONE;
+    PENDING("아직 대기 상태예요\uD83D\uDE42"),
+    ACCEPT("제가 처리할게요😎"),
+    DENY("지금은 처리하기 어려워요 \uD83D\uDE22"),
+    DONE("처리 완료했습니다\uD83E\uDD70");
+
+    private final String message;
+
+    ManagerStatus(String message) {
+        this.message = message;
+    }
 
     public static ManagerStatus of(String status) {
         if (status == null) return null;
