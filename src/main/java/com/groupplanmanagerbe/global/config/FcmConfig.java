ccd191ffe774b187bcd7620fbaf28b1050daeed2
goo3oo <goo3oo0918@gmail.com>
2025-09-06 11:02:46 +0900

    feat: fcm 설정, 서비스

---
+++ src/main/java/com/groupplanmanagerbe/global/config/FcmConfig.java
@@ -0,0 +1,29 @@
+package com.groupplanmanagerbe.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FcmConfig {
+
+    @Value("${firebase.service-account-path}")
+    private String serviceAccountPath;
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream(serviceAccountPath);
+
+        FirebaseOptions options =  FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+}
