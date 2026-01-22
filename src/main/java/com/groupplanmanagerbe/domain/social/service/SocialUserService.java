edcb97ed32697e6707d303ccf10894a498b1481f
goo3oo <goo3oo0918@gmail.com>
2026-01-22 16:56:02 +0900

    fix: 구글 소셜로그인 로깅 추가

--- src/main/java/com/groupplanmanagerbe/domain/social/service/SocialUserService.java
+++ src/main/java/com/groupplanmanagerbe/domain/social/service/SocialUserService.java
@@ -16,6 +16,7 @@ import com.groupplanmanagerbe.presentation.social.dto.model.provider.KakaoUser;
 import com.groupplanmanagerbe.presentation.social.dto.model.provider.NaverUser;
 import com.groupplanmanagerbe.presentation.social.dto.model.provider.ProviderUser;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -26,6 +27,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -42,8 +44,23 @@ public class SocialUserService {
     @Transactional
     public TokenRes socialLogin(String token, SocialProvider provider) throws FirebaseAuthException {
         ProviderUser providerUser;
+        log.info("[SOCIAL_LOGIN] provider={} start", provider);
         switch (provider) {
-            case GOOGLE -> providerUser = GoogleUser.of(FirebaseAuth.getInstance().verifyIdToken(token));
+            case GOOGLE -> {
+                log.info("[Google] start");
+
+                try {
+                    var decodedToken =
+                            FirebaseAuth.getInstance().verifyIdToken(token);
+
+                    log.info("[Google] token verified");
+                    providerUser = GoogleUser.of(FirebaseAuth.getInstance().verifyIdToken(token));
+                    log.info("[GOOGLE] mapping done");
+                } catch (FirebaseAuthException e) {
+                    log.warn("[GOOGLE] verify fail", e);
+                    throw e;
+                }
+            }
             case NAVER -> providerUser = NaverUser.of(getResponse(token, NAVER_USER_INFO_URL));
             case KAKAO -> providerUser = KakaoUser.of(getResponse(token, KAKAO_USER_INFO_URL));
             default -> throw new InvalidException(ApiErrorCode.PROVIDER_INVALID);
@@ -60,6 +77,7 @@ public class SocialUserService {
     private Map<String, Object> getResponse(String accessToken, String infoUrl) {
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(accessToken);
+        log.info("헤더 받아옴");
         HttpEntity<Void> httpRequest = new HttpEntity<>(headers);
         return restTemplate.exchange(
                 infoUrl,
@@ -71,7 +89,9 @@ public class SocialUserService {
     }
 
     private SocialUser createNewSocialUser(ProviderUser provider) {
+        log.info("신규 유저 등록점 진입");
         User user = findOrCreateUser(provider.getEmail(), provider.getNickName());
+        log.info("유저 검색, 생성 성공");
         return socialUserRepository.save(SocialUser.of(user, provider.getProvider(), provider.getProviderId()));
     }
 
@@ -82,6 +102,7 @@ public class SocialUserService {
     }
 
     private TokenRes createTokenRes(User user) {
+        log.info("토근 생성 진입 성공");
         String accessToken = jwtUtil.createAccessToken(user.getId(), user.getRole());
         String refreshToken = jwtUtil.createRefreshToken(user.getId());
         return TokenRes.of(accessToken, refreshToken);
